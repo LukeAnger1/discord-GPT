ea2d3da1e74ed43c3599ae53f832bc350e2d4efc
Luke Anger <anger.luke2004@gmail.com>
2024-03-12 17:42:23 -0400

    installation sh, currently doesnt work. Going to follow youtube video

---
+++ install-gpt-wrapper.sh
@@ -0,0 +1,10 @@
+# credit to the below repo for the chatGPT repo
+# https://github.com/mindgitrwx/chatgpt-wrapper
+
+# Install the latest version of this software directly from github with pip:
+pip install git+https://github.com/mmabrouk/chatgpt-wrapper
+# Install a browser in playwright (if you haven't already). The program will use firefox by default.
+playwright install firefox
+# Start up the program in install mode. This opens up a browser window. Log in to ChatGPT in the browser window, then stop the program.
+chatgpt install
+# Restart the program without the install parameter to begin using it.
