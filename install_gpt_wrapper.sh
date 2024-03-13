77048394f88d5196e0b0cbd96c1f8b69c7e72c07
Luke Anger <anger.luke2004@gmail.com>
2024-03-13 17:03:20 -0400

    saving work

---
+++ install_gpt_wrapper.sh
@@ -0,0 +1,9 @@
+
+# This installs the software for python environment
+sudo apt install python3.10-venv -y
+
+# This creates the environment
+python3 -m venv ./chatgpt_env
+
+# This will start the linux env
+source chatgpt_env/bin/activate
